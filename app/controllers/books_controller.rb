5883252d20197c797f49aaa07c45f76b702f68f2
Karen <kjgamis@live.ca>
2018-05-07 14:29:13 -0400

    add plain text response

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,14 @@
 class BooksController < ApplicationController
 
   def index
+    @books = Book.all
 
     respond_to do |format|
       format.html
+      format.text
     end
 
-    
   end
+
+
 end
